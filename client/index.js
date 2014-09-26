035c4af736bb6c00fb37a3a781c7b55933fd5cec
Nizar Sehli <nse@zenexity.com>
2014-09-26 10:17:48 +0200

    Drag&drop module

--- client/index.js
+++ client/index.js
@@ -1,4 +1,8 @@
 var GlslTransition = require("glsl-transition");
 var GlslTransitions = require("glsl-transitions");
 
+var dragdrop        = require("./dragdrop");
+console.log(dragdrop());
+
 console.log(process.env.SERVER);
+
